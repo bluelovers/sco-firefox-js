fdf43e71fe0904bca2da12b15a5757cb80b8d55b
bluelovers <codelovers@users.sourceforge.net>
2015-02-22 01:11:46 +0800

    ProtocolChannel

---
+++ Protocol/Channel.js
@@ -0,0 +1,29 @@
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+this.EXPORTED_SYMBOLS = ['ProtocolChannel'];
+this.exports = {ProtocolChannel};
+
+function ProtocolChannel()
+{
+	return new ProtocolChannel.prototype.__construct();
+};
+
+Object.assign(ApiObserver.prototype,
+{
+
+	__construct: function()
+	{
+		var _this = this;
+
+		return this;
+	},
+
+});
+
+ProtocolChannel.prototype.__construct.prototype = ProtocolChannel.prototype;
+
+return this.exports;
