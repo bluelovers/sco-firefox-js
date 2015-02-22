666a3b9bc813d347e0de3195aa59653354061c34
bluelovers <codelovers@users.sourceforge.net>
2015-02-23 01:51:50 +0800

    . (Step: 1)
    Step: 1
    
    [A ] new file:
    
    	.demo/bootstrap.js

---
+++ .demo/bootstrap.js
@@ -0,0 +1,37 @@
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+const Cr = Components.results;
+const Cu = Components.utils;
+
+(function(){
+
+	var {Bootstrap} = module('Bootstrap.js');
+
+	function module(uri, base)
+	{
+		if (!/^[a-z-]+:/.exec(uri))
+		{
+			let ext = fileext(uri);
+
+			if (!(ext === 'js' || ext === 'jsm'))
+			{
+				uri += '.jsm'
+			}
+
+			uri = (base || /([^ ]+\/)[^\/]+$/.exec(Components.stack.caller.filename)[1] + '../') + uri;
+		}
+
+		let obj = {};
+		Cu.import(uri, obj);
+		return obj;
+	}
+
+	function fileext(file)
+	{
+		(file + '').match(/\.([^\.]+)$/);
+
+		return RegExp.$1;
+	}
+
+})();
